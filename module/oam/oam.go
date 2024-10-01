babb3e0cda17eb64c2bc0ca6969eb5673702ad15
Donny Hardyanto <hardyanto.donny@gmail.com>
2024-10-01 23:48:48 +0700

    - Six commit

--- module/oam/oam.go
+++ module/oam/oam.go
@@ -10,11 +10,14 @@ func DefineAPIEndPoints(anAPI *api.DXAPI) (err error) {
 	anAPI.NewEndPoint("PING",
 		"Receive Ping and send out Pong. Used to indicate the service is active and well.",
 		"/ping", "GET", api.EndPointTypeHTTPJSON, http.ContentTypeApplicationJSON, nil,
-		oam.Ping, nil, nil, nil)
+		oam.Ping, nil, nil, nil, nil,
+	)
 
 	anAPI.NewEndPoint("PrintSpec",
 		"Print the API Specification",
 		"/spec", "GET", api.EndPointTypeHTTPJSON, http.ContentTypeApplicationJSON, nil,
-		anAPI.APIHandlerPrintSpec, nil, nil, nil)
+		anAPI.APIHandlerPrintSpec, nil, nil, nil, nil,
+	)
+
 	return nil
 }
