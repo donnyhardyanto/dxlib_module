4acfbcda919f61f837b2bc13909566460603dd53
Donny Hardyanto <hardyanto.donny@gmail.com>
2024-11-21 18:02:22 +0700

    Update API endpoint names and descriptions for clarity
    Renamed and enhanced several API endpoint names and descriptions to improve clarity and consistency. Updated endpoint methods to include detailed explanations and structured information, ensuring better understanding and usability for developers. Also added a missing organization list API call in an automated test script and included a new CSV file for attachment testing.

--- module/user_management/user_management_organization.go
+++ module/user_management/user_management_organization.go
@@ -1,12 +1,67 @@
 package user_management
 
 import (
+	"fmt"
 	"github.com/donnyhardyanto/dxlib/api"
 	"github.com/donnyhardyanto/dxlib/utils"
 )
 
 func (um *DxmUserManagement) OrganizationList(aepr *api.DXAPIEndPointRequest) (err error) {
-	return um.Organization.RequestPagingList(aepr)
+	isExistFilterWhere, filterWhere, err := aepr.GetParameterValueAsString("filter_where")
+	if err != nil {
+		return err
+	}
+	if !isExistFilterWhere {
+		filterWhere = ""
+	}
+	isExistFilterOrderBy, filterOrderBy, err := aepr.GetParameterValueAsString("filter_order_by")
+	if err != nil {
+		return err
+	}
+	if !isExistFilterOrderBy {
+		filterOrderBy = ""
+	}
+
+	isExistFilterKeyValues, filterKeyValues, err := aepr.GetParameterValueAsJSON("filter_key_values")
+	if err != nil {
+		return err
+	}
+	if !isExistFilterKeyValues {
+		filterKeyValues = nil
+	}
+
+	t := um.Organization
+
+	_, isDeletedIncluded, err := aepr.GetParameterValueAsBool("is_deleted", false)
+	if err != nil {
+		return err
+	}
+
+	if !isDeletedIncluded {
+		if filterWhere != "" {
+			filterWhere = fmt.Sprintf("(%s) and ", filterWhere)
+		}
+
+		switch t.Database.DatabaseType.String() {
+		case "sqlserver":
+			filterWhere = filterWhere + "(is_deleted=0)"
+		case "postgres":
+			filterWhere = filterWhere + "(is_deleted=false)"
+		default:
+			filterWhere = filterWhere + "(is_deleted=0)"
+		}
+	}
+
+	return t.DoRequestPagingList(aepr, filterWhere, filterOrderBy, filterKeyValues, func(listRow utils.JSON) (utils.JSON, error) {
+		organizationId := listRow[`id`].(int64)
+		_, organizationRoles, err := um.OrganizationRoles.Select(&aepr.Log, nil, utils.JSON{`organization_id`: organizationId}, map[string]string{"id": "asc"}, nil)
+		if err != nil {
+			return listRow, err
+		}
+		listRow["organization_roles"] = organizationRoles
+		return listRow, nil
+	})
+
 }
 
 func (um *DxmUserManagement) OrganizationCreate(aepr *api.DXAPIEndPointRequest) (err error) {
