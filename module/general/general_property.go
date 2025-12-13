a9cf84c3fd34350ff1350e6e84fc92dba58fd9c1
Donny Hardyanto <3777933-donny.hardyanto@users.noreply.gitlab.com>
2025-12-13 10:43:05 +0700

    Enhance template handling: add TemplateGetByNameId method and clean up unused template definitions

--- module/general/general_property.go
+++ module/general/general_property.go
@@ -1,9 +1,12 @@
 package general
 
 import (
+	"github.com/donnyhardyanto/dxlib/log"
 	dxlibModule "github.com/donnyhardyanto/dxlib/module"
 	"github.com/donnyhardyanto/dxlib/table"
+	"github.com/donnyhardyanto/dxlib/utils"
 	"github.com/donnyhardyanto/dxlib_module/lib"
+	"github.com/pkg/errors"
 )
 
 type DxmGeneral struct {
@@ -11,10 +14,7 @@ type DxmGeneral struct {
 	Property            *table.DXPropertyTable
 	Announcement        *table.DXTable
 	AnnouncementPicture *lib.ImageObjectStorage
-	/*	EMailTemplate       *table.DXTable
-		SMSTemplate         *table.DXTable
-		FCMTemplate         *table.DXTable*/
-	Template *table.DXTable
+	Template            *table.DXTable
 }
 
 func (g *DxmGeneral) Init(databaseNameId string) {
@@ -25,19 +25,29 @@ func (g *DxmGeneral) Init(databaseNameId string) {
 	g.Announcement = table.Manager.NewTable(databaseNameId, "general.announcement",
 		"general.announcement",
 		"general.announcement", "uid", "id", "uid", "data")
-	/*g.EMailTemplate = table.Manager.NewTable(g.DatabaseNameId,
-		"general.email_template", "general.email_template",
-		"general.email_template", "nameid", "id", "uid", "data")
-	g.SMSTemplate = table.Manager.NewTable(g.DatabaseNameId,
-		"general.sms_template", "general.sms_template",
-		"general.sms_template", "nameid", "id", "uid", "data")
-	g.FCMTemplate = table.Manager.NewTable(g.DatabaseNameId,
-		"general.fcm_template", "general.fcm_template",
-		"general.fcm_template", "nameid", "id", "uid", "data")
-	*/
 	g.Template = table.Manager.NewTable(g.DatabaseNameId,
 		"general.template", "general.template",
 		"settings.template", "nameid", "id", "uid", "data")
+}
+
+func (g *DxmGeneral) TemplateGetByNameId(l *log.DXLog, nameId string) (gt utils.JSON, templateTitle string, templateContentType string, templateBody string, err error) {
+	_, templateMessage, err := g.Template.ShouldGetByNameId(l, nameId)
+	if err != nil {
+		return nil, "", "", "", err
+	}
+	templateTitle, ok := templateMessage["subject"].(string)
+	if !ok {
+		return nil, "", "", "", errors.New("INVALID_TEMPLATE_TITLE")
+	}
+	templateContentType, ok = templateMessage["content_type"].(string)
+	if !ok {
+		return nil, "", "", "", errors.New("INVALID_TEMPLATE_CONTENT_TYPE")
+	}
+	templateBody, ok = templateMessage["body"].(string)
+	if !ok {
+		return nil, "", "", "", errors.New("INVALID_TEMPLATE_BODY")
+	}
+	return templateMessage, templateTitle, templateContentType, templateBody, nil
 }
 
 var ModuleGeneral DxmGeneral
