db859f8ceba4d546ede178723c30154698c367fb
Donny Hardyanto <3777933-donny.hardyanto@users.noreply.gitlab.com>
2026-02-04 10:09:26 +0700

    Refactor API to replace filter_where and filter_order_by with EqOrIn for safer query handling

--- module/user_management/user_management_privilege.go
+++ module/user_management/user_management_privilege.go
@@ -1,14 +1,7 @@
 package user_management
 
 import (
-	"fmt"
-	"net/http"
-	"strings"
-	"time"
-
 	"github.com/donnyhardyanto/dxlib/api"
-	"github.com/donnyhardyanto/dxlib/databases/db"
-	"github.com/donnyhardyanto/dxlib/databases/export"
 )
 
 func (um *DxmUserManagement) PrivilegeCreate(aepr *api.DXAPIEndPointRequest) (err error) {
@@ -18,102 +11,4 @@ func (um *DxmUserManagement) PrivilegeCreate(aepr *api.DXAPIEndPointRequest) (er
 		"description": aepr.ParameterValues["description"].Value.(string),
 	})
 	return err
-}
-
-func (um *DxmUserManagement) PrivilegeListDownload(aepr *api.DXAPIEndPointRequest) (err error) {
-	isExistFilterWhere, filterWhere, err := aepr.GetParameterValueAsString("filter_where")
-	if err != nil {
-		return err
-	}
-	if !isExistFilterWhere {
-		filterWhere = ""
-	}
-	isExistFilterOrderBy, filterOrderBy, err := aepr.GetParameterValueAsString("filter_order_by")
-	if err != nil {
-		return err
-	}
-	if !isExistFilterOrderBy {
-		filterOrderBy = ""
-	}
-
-	isExistFilterKeyValues, filterKeyValues, err := aepr.GetParameterValueAsJSON("filter_key_values")
-	if err != nil {
-		return err
-	}
-	if !isExistFilterKeyValues {
-		filterKeyValues = nil
-	}
-
-	_, format, err := aepr.GetParameterValueAsString("format")
-	if err != nil {
-		return aepr.WriteResponseAndNewErrorf(http.StatusBadRequest, "FORMAT_PARAMETER_ERROR:%s", err.Error())
-	}
-
-	format = strings.ToLower(format)
-
-	t := um.Privilege
-
-	isDeletedIncluded := false
-	if !isDeletedIncluded {
-		if filterWhere != "" {
-			filterWhere = fmt.Sprintf("(%s) and ", filterWhere)
-		}
-
-		if err := t.Database.EnsureConnection(); err != nil {
-			return err
-		}
-
-		switch t.Database.DatabaseType.String() {
-		case "sqlserver":
-			filterWhere = filterWhere + "(is_deleted=0)"
-		case "postgres":
-			filterWhere = filterWhere + "(is_deleted=false)"
-		default:
-			filterWhere = filterWhere + "(is_deleted=0)"
-		}
-	}
-
-	if err := t.Database.EnsureConnection(); err != nil {
-		return err
-	}
-
-	rowsInfo, list, err := db.NamedQueryList(t.Database.Connection, t.FieldTypeMapping, "*", t.ListViewNameId,
-		filterWhere, "", filterOrderBy, filterKeyValues)
-
-	if err != nil {
-		return err
-	}
-
-	// Set export options
-	opts := export.ExportOptions{
-		Format:     export.ExportFormat(format),
-		SheetName:  "Sheet1",
-		DateFormat: "2006-01-02 15:04:05",
-	}
-
-	// Get file as stream
-	data, contentType, err := export.ExportToStream(rowsInfo, list, opts)
-	if err != nil {
-		return err
-	}
-
-	// Set response headers
-	filename := fmt.Sprintf("export_%s_%s.%s", t.ListViewNameId, time.Now().Format("20060102_150405"), format)
-
-	responseWriter := *aepr.GetResponseWriter()
-	responseWriter.Header().Set("Content-Type", contentType)
-	responseWriter.Header().Set("Content-Length", fmt.Sprintf("%d", len(data)))
-	responseWriter.Header().Set("Content-Disposition", fmt.Sprintf("attachment; filename=\"%s\"", filename))
-	responseWriter.WriteHeader(http.StatusOK)
-	aepr.ResponseStatusCode = http.StatusOK
-
-	_, err = responseWriter.Write(data)
-	if err != nil {
-		return err
-	}
-
-	aepr.ResponseHeaderSent = true
-	aepr.ResponseBodySent = true
-
-	return nil
 }
